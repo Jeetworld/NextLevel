0fdbe04ebbb9857e983f23cfd474a75b69bc0713
Jeetworld <singhkamaljit64@gmail.com>
2019-04-09 09:45:27 -0500

    Yot file pushed

---
+++ Yot.java
@@ -0,0 +1,18 @@
+package FirstPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Yot {
+
+	@Test
+	public void abc()
+	{		
+		
+		System.out.println("QWERTY");
+	}
+
+}
